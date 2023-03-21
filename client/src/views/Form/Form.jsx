6afa6dedb043ccdc51bee5d6f409c72a94008a03
Mikmoal <miguel.moam@outlook.com>
2023-03-21 15:43:05 -0600

    form de nuevo pokemon

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,10 +1,74 @@
+import { useState } from "react";
 function Form() {
-    return (
-      <div className="Form">
-        <h1>View of Form page</h1>
-        
-      </div>
-    );
+  const [form, setForm] = useState({
+    name: "",
+    image: "",
+    life: "",
+    attack: "",
+    defense: "",
+    speed: "",
+    height: "",
+    weight: "",
+    types: []
+  })
+
+  const changeHandler = (event) => {
+    const property = event.target.name
+    const value = event.target.value
+
+    setForm({ ...form, [property]: value })
   }
-  
-  export default Form;
+
+  return (
+    <form>
+      <div>
+        <label>Nombre</label>
+        <input type="text" value={form.name} onChange={changeHandler} name="name" />
+      </div>
+
+      <div>
+        <label>Imagen</label>
+        <input type="text" value={form.image} onChange={changeHandler} name="image" />
+      </div>
+
+      <div>
+        <label>Vida</label>
+        <input type="text" value={form.life} onChange={changeHandler} name="life" />
+      </div>
+
+      <div>
+        <label>Ataque</label>
+        <input type="text" value={form.attack} onChange={changeHandler} name="attack" />
+      </div>
+
+      <div>
+        <label>Defensa</label>
+        <input type="text" value={form.defense} onChange={changeHandler} name="defense" />
+      </div>
+
+      <div>
+        <label>Velocidad (si tiene)</label>
+        <input type="text" value={form.speed} onChange={changeHandler} name="speed" />
+      </div>
+
+      <div>
+        <label>Altura (si tiene)</label>
+        <input type="text" value={form.height} onChange={changeHandler} name="height" />
+      </div>
+
+      <div>
+        <label>Peso (si tiene)</label>
+        <input type="text" value={form.weight} onChange={changeHandler} name="weight" />
+      </div>
+
+      <div>
+        <label>Tipo(s)</label>
+        <input type="text" value={form.types} onChange={changeHandler} name="types" />
+      </div>
+
+      <button>Submit</button>
+    </form>
+  )
+}
+
+export default Form;
