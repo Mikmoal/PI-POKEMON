663cbb193e627e6d586b8312c477c878d2ea66c3
Mikmoal <miguel.moam@outlook.com>
2023-04-05 19:40:25 -0600

    se renderiza detail

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -3,9 +3,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPokemonId } from "../../redux/actions";
 import loading from "../../loading.gif";
 
+function renderizarDetail(pokemonDetailed) {
+  // console.log("se ejecuta renderizarDetail");
+  const types = pokemonDetailed.types.map((element) => {
+    return element.type.name;
+  });
+  const hpIndex = pokemonDetailed.stats.findIndex(
+    (el) => el.stat.name === "hp"
+  );
+  const attackIndex = pokemonDetailed.stats.findIndex(
+    (el) => el.stat.name === "attack"
+  );
+  const defenseIndex = pokemonDetailed.stats.findIndex(
+    (el) => el.stat.name === "defense"
+  );
+  const speedIndex = pokemonDetailed.stats.findIndex(
+    (el) => el.stat.name === "speed"
+  );
+
+  return (
+    <div key={pokemonDetailed.id}>
+      <img src={pokemonDetailed.sprites.other.home.front_default} alt={pokemonDetailed.name} />
+      <p>ID: {pokemonDetailed.id}</p>
+      <p>Name: {pokemonDetailed.name}</p>
+      <p>Life: {pokemonDetailed.stats[hpIndex].base_stat}</p>
+      <p>Attack: {pokemonDetailed.stats[attackIndex].base_stat}</p>
+      <p>Defense: {pokemonDetailed.stats[defenseIndex].base_stat}</p>
+      <p>Speed: {pokemonDetailed.stats[speedIndex].base_stat}</p>
+      <p>Height: {pokemonDetailed.height}</p>
+      <p>Weight: {pokemonDetailed.weight}</p>
+      <p>Types: {types.join(', ')}</p>
+    </div>
+  )
+}
 
 export default function Detail(props) {
-  //const types = props.types.join(', ');
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,56 +46,20 @@ export default function Detail(props) {
   }, [dispatch, props.match.params.id])
 
   const pokemonDetailed = useSelector((state) => state.pokemonsDetail)
-  console.log(Array.isArray(pokemonDetailed))
-  console.log("este es lo que trae del state: " + pokemonDetailed)
+  // console.log(Array.isArray(pokemonDetailed))
+  // console.log("este es lo que trae del state: " + pokemonDetailed)
 
-  const pokemon = []
-  pokemon.push(pokemonDetailed);
-  console.log(pokemon)
+
+
   return (
+
     <div className="Detail">
       <div >
-        {pokemon.length === 0 ? (
-          <div><img src={loading} width='120px' alt='loading' /> </div>) :
-          (
-            <div>
-              {pokemon.map(el => {
-                console.log(el.types)
-                const types = el.types.map((element) => {
-                  return element.type.name;
-                });
-            
-                const hpIndex = pokemon.stats.findIndex(
-                  (el) => el.stat.name === "hp"
-                );
-                const attackIndex = pokemon.stats.findIndex(
-                  (el) => el.stat.name === "attack"
-                );
-                const defenseIndex = pokemon.stats.findIndex(
-                  (el) => el.stat.name === "defense"
-                );
-                const speedIndex = pokemon.stats.findIndex(
-                  (el) => el.stat.name === "speed"
-                );
-
-                return (
-                  <div key={el.id}>
-                    <img src={el.image} alt={el.name} />
-                    <p>ID: {el.id}</p>
-                    <p>Name: {el.name}</p>
-                    <p>Life: {el.stats[hpIndex].base_stat}</p>
-                    <p>Attack: {el.stats[attackIndex].base_stat}</p>
-                    <p>Defense: {el.stats[defenseIndex].base_stat}</p>
-                    <p>Speed: {el.stats[speedIndex].base_stat}</p>
-                    <p>Height: {el.height}</p>
-                    <p>Weight: {el.weight}</p>
-                    <p>Types: {types.join(', ')}</p>
-                  </div>
-                )
-              })}
-            </div>
-          )}
+        {!pokemonDetailed.id ? (
+          <div><img src={loading} width='120px' alt='loading' /> </div>) : renderizarDetail(pokemonDetailed)
+        }
       </div>
     </div>
+    
   );
 }
